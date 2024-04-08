5d156fe0e481b47101ce73061bdf72861ac3e82b
sandhiaakib <aakib.sandhi@calidadinfotech.com>
2024-04-08 14:54:02 +0530

    sandhi

---
+++ whileloops.py
@@ -0,0 +1,15 @@
+# x = 0
+# while x <10:
+#     print('value of x is ' +str(x))
+#     x = x+1
+
+
+l = []
+num = 0
+while num <10:
+    l.append(num)
+   
+    print('value of num is '+str (num))
+    num += 1
+
+print(l)
